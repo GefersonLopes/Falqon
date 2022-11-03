4f87ec3a63c9646cb85e422519f76ef31889e74a
neyprodigio <103160734+GefersonLopes@users.noreply.github.com>
2022-11-03 19:50:41 -0300

    production config

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,11 +3,11 @@ import "dotenv/config";
 
 export const AppDataSource = new DataSource({
         type: "postgres",
-        url: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL, 
         ssl: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false} : false,
 
         logging: true,
-        synchronize: true,
+        synchronize: false,
         
         entities: process.env.NODE_ENV === "production" ? ["dist/src/entities/*.js"] : ["src/entities/*.ts"],
         migrations: process.env.NODE_ENV === "production" ? ["dist/src/migrations/*.js"] : ["src/migrations/*.ts"],
