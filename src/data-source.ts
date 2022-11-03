e2d9ab2365a63e26e3a7de3996a1ccf3b2f87112
neyprodigio <103160734+GefersonLopes@users.noreply.github.com>
2022-11-03 19:29:31 -0300

    create routes in heroku

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,18 +3,14 @@ import "dotenv/config";
 
 export const AppDataSource = new DataSource({
         type: "postgres",
-        host: "localhost",
-        port: 5432,
-
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB,
+        url: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false} : false,
 
         logging: true,
         synchronize: true,
         
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
+        entities: process.env.NODE_ENV === "production" ? ["dist/src/entities/*.js"] : ["src/entities/*.ts"],
+        migrations: process.env.NODE_ENV === "production" ? ["dist/src/migrations/*.js"] : ["src/migrations/*.ts"],
       }
 );
 
