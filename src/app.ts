a9f904fd72a8853297ff7f2ac00b926d08f91fb0
neyprodigio <103160734+GefersonLopes@users.noreply.github.com>
2022-11-04 14:07:21 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,14 @@ import { error } from "./middlewares/Error";
 import { Tasks_Router } from "./routes/tasks.routes";
 import { User_Router } from "./routes/user.routes";
 
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     app.use(cors());
     next();
 });
