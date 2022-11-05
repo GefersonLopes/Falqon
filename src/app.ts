826b9d91eb94798937a96c93e47bbca2a170bec7
neyprodigio <103160734+GefersonLopes@users.noreply.github.com>
2022-11-04 22:19:18 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
     res.setHeader(
         "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
     );
     app.use(cors());
     next();
