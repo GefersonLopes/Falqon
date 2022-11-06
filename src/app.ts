52eeacbb4e1dc23ef061bf175df401d8164694ee
Geferson <gefersonjefreey@gmail.com>
2022-11-05 23:49:15 -0300

    fix update cors

--- src/app.ts
+++ src/app.ts
@@ -10,17 +10,21 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use(require('cors')());
+/* app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
+    );
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
     );
     app.use(cors());
     next();
-});
+}); */
 
 app.use("/tasks", Tasks_Router);
 app.use("/user", User_Router);
